9d9b03e837b09fec0afe674c0914973c8b6e694e
luisarmandoob <lolvertorsiete@gmail.com>
2026-04-29 08:16:34 -0700

    Add files via upload

---
+++ videojuegos (1).sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-04-2026 a las 17:14:50
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `videojuegos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `genero`
+--
+
+CREATE TABLE `genero` (
+  `id_genero` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`id_genero`, `nombre`, `descripcion`) VALUES
+(1, 'aventura', 'trata de pasar niveles ya que es un modo historia '),
+(2, 'shotter', 'trata de disparos y sobrevivir'),
+(3, 'terror', 'trata de sustos y suspenso'),
+(5, 'RPG', 'es un juego de rol donde asumes el papel de un personaje'),
+(6, 'accion', 'trata de ataques , poderes, peleas');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plataforma`
+--
+
+CREATE TABLE `plataforma` (
+  `id_plataforma` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `fabricante` varchar(80) NOT NULL,
+  `año_lanzamiento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `plataforma`
+--
+
+INSERT INTO `plataforma` (`id_plataforma`, `nombre`, `fabricante`, `año_lanzamiento`) VALUES
+(1, 'PlayStation 4', 'Sony', '2013-01-01'),
+(2, 'PlayStation 5', 'Sony', '2020-01-01'),
+(3, 'Xbox Series X', 'Microsoft', '2020-01-01'),
+(4, 'Nintendo Switch 2', 'Nintendo', '2025-01-01');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videojuego`
+--
+
+CREATE TABLE `videojuego` (
+  `id_juego` int(11) NOT NULL,
+  `titulo` varchar(120) NOT NULL,
+  `desarrolladora` varchar(100) NOT NULL,
+  `año_lanzamiento` date NOT NULL,
+  `precio` decimal(6,0) NOT NULL,
+  `id_genero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `videojuego`
+--
+
+INSERT INTO `videojuego` (`id_juego`, `titulo`, `desarrolladora`, `año_lanzamiento`, `precio`, `id_genero`) VALUES
+(1, 'Uncharted 4', 'Naughty Dog', '2016-01-01', 1000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videojuego_plataforma`
+--
+
+CREATE TABLE `videojuego_plataforma` (
+  `id_juego` int(11) NOT NULL,
+  `id_plataforma` int(11) NOT NULL,
+  `fecha_disponible` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `genero`
+--
+ALTER TABLE `genero`
+  ADD PRIMARY KEY (`id_genero`);
+
+--
+-- Indices de la tabla `plataforma`
+--
+ALTER TABLE `plataforma`
+  ADD PRIMARY KEY (`id_plataforma`);
+
+--
+-- Indices de la tabla `videojuego`
+--
+ALTER TABLE `videojuego`
+  ADD PRIMARY KEY (`id_juego`),
+  ADD KEY `id_genero` (`id_genero`);
+
+--
+-- Indices de la tabla `videojuego_plataforma`
+--
+ALTER TABLE `videojuego_plataforma`
+  ADD KEY `id_plataforma` (`id_plataforma`),
+  ADD KEY `id_juego` (`id_juego`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `genero`
+--
+ALTER TABLE `genero`
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `plataforma`
+--
+ALTER TABLE `plataforma`
+  MODIFY `id_plataforma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `videojuego`
+--
+ALTER TABLE `videojuego`
+  MODIFY `id_juego` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `videojuego`
+--
+ALTER TABLE `videojuego`
+  ADD CONSTRAINT `videojuego_ibfk_1` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`);
+
+--
+-- Filtros para la tabla `videojuego_plataforma`
+--
+ALTER TABLE `videojuego_plataforma`
+  ADD CONSTRAINT `videojuego_plataforma_ibfk_1` FOREIGN KEY (`id_juego`) REFERENCES `videojuego` (`id_juego`),
+  ADD CONSTRAINT `videojuego_plataforma_ibfk_2` FOREIGN KEY (`id_plataforma`) REFERENCES `plataforma` (`id_plataforma`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
